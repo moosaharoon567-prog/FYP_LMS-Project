a700aee5dd3546efb06e2f7ea788536f493c77e8
moosaharoon567-prog <moosa.haroon567@gmail.com>
2026-09-08 13:22:44 +0500

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -86,6 +86,14 @@ function App() {
         }
       />
       <Route
+        path="/quizzes/:id/submissions"
+        element={
+          <ProtectedRoute allowedRoles={['teacher', 'admin']}>
+            <QuizSubmissions />
+          </ProtectedRoute>
+        }
+      />
+      <Route
         path="/users"
         element={
           <ProtectedRoute allowedRoles={['admin']}>
